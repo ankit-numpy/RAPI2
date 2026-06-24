5972f4bad6d07f9e4c7721646360eb33c27cf21a
Ankit Dixit <88057272+ankit-numpy@users.noreply.github.com>
2026-06-24 13:09:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,7 @@
-async function query(data) {
-    const response = await fetch(
-        "https://cloud.flowiseai.com/api/v1/prediction/3563ab8d-18b5-4875-bf56-6ee0992a5d1b",
-        {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(data)
-        }
-    );
-    const result = await response.json();
-    return result;
-}
-
-query({"question": "Hey, how are you?"}).then((response) => {
-    console.log(response);
-});
+<script type="module">
+    import Chatbot from "https://cdn.jsdelivr.net/npm/flowise-embed/dist/web.js"
+    Chatbot.init({
+        chatflowid: "3563ab8d-18b5-4875-bf56-6ee0992a5d1b",
+        apiHost: "https://cloud.flowiseai.com",
+    })
+</script>
